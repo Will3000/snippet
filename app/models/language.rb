f0830274b48e0ed4c52e39469d9e8429fe24aeaf
Wilson Lu <huiquanlu@gmail.com>
2016-06-30 15:32:09 -0700

    create language controller and show page

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,5 @@
 class Language < ActiveRecord::Base
   has_many :snippets, dependent: :nullify
+
+  validates :name, uniqueness: true, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :snippets
+  resources :languages, only: [:show]
 
   root "snippets#index"
   # The priority is based upon order of creation: first created -> highest priority.
